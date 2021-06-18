2f6532b38e6f4c8b210d01912478254e73ff8271
Jon Radchenko <jonrad@gmail.com>
2021-06-18 15:09:21 -0400

    missing path

--- gitpod/install.sh
+++ gitpod/install.sh
@@ -1,10 +1,10 @@
 sudo chown -R gitpod /theia/theia-app
 npm install -g nvm
-cp /workspace/theia-server-proxy-extension/gitpod/package.json /theia/theia-app/app/
+cp /home/gitpod/theia/gitpod/package.json /theia/theia-app/app/
 nvm install 12
 nvm use 12
 node -v
 cd /theia/theia-app
 yarn
-cp /workspace/theia-server-proxy-extension/gitpod/startup.sh /theia/theia-app/app/
+cp /home/gitpod/theia/gitpod/startup.sh /theia/theia-app/app/
 chmod 755 /theia/theia-app/app/startup.sh
